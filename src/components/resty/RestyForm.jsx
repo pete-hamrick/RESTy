13fae55b5e6c6d5284cd1b71cf13a9c8858d6ab0
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-04 15:35:45 -0700

    built out form

--- src/components/resty/RestyForm.jsx
+++ src/components/resty/RestyForm.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function RestyForm() {
-  return <h1>RestyForm</h1>;
+  return (
+    <form>
+      <label htmlFor="urlInput">Add Url Here: </label>
+      {/* TODO Add value and onChange to this input */}
+      <input id="urlInput" name="urlInput" placeholder="URL"></input>
+
+      <input id="GET" type="radio" name="methodSelector"></input>
+      <label for="GET">GET</label>
+      <input id="POST" type="radio" name="methodSelector"></input>
+      <label for="POST">POST</label>
+      <input id="PUT" type="radio" name="methodSelector"></input>
+      <label for="PUT">PUT</label>
+      <input id="PATCH" type="radio" name="methodSelector"></input>
+      <label for="PATCH">PATCH</label>
+      <input id="DELETE" type="radio" name="methodSelector"></input>
+      <label for="DELETE">DELETE</label>
+
+      <button aria-label="goButton">Go</button>
+
+      <textarea></textarea>
+    </form>
+  );
+  // Input
+  // Radio Button Methods
+  // Button
+  // Text Area for JSON
 }
 
 export default RestyForm;
