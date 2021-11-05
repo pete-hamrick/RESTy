1b78a378a4e6732171274747162f56cd247edf40
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-05 11:02:10 -0700

    added state, pseudo code functions for handling input, submit, and my fetchUtil

--- src/Containers/RestyContainer.jsx
+++ src/Containers/RestyContainer.jsx
@@ -7,6 +7,29 @@ import RestyHeader from '../components/resty/RestyHeader';
 export default class RestyCage extends Component {
   // state:
   // urlInput, selectedMethod, history, response, loading, jsonHeader
+  state = {
+    loading: true,
+    urlInput: '',
+    method: '',
+    history: [],
+    response: {}, //string maybe? array?
+    jsonInput: '',
+  };
+
+  // compdidmount
+  // set state loading false
+  // componentDidMount() {
+  //   this.setState({ loading: false });
+  // }
+
+  // handle input
+  // set state to target value
+
+  // handle submit async passing urlInput, method, jsonInput
+  // prevent default
+  // set state loading true
+  // await fetchRequest passing urlInput, method, jsonInput
+  // set state loading false,
   render() {
     return (
       <>
